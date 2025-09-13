f006c1c82634b0fdbca3d245abdb7e7d009b3e91
Forest Agostinelli <forestagostinelli@Forests-MacBook-Air.local>
2025-09-13 15:47:50 +0100

    examples [no ci]

--- examples/davi_bwas.sh
+++ examples/davi_bwas.sh
@@ -3,4 +3,4 @@
 # Rubik's cube
 python run_davi.py --env cube3 --step_max 30 --nnet_dir models/cube3/ --batch_size 10000 --up_itrs 5000 --up_procs 48 --up_step_max 30 --rb 10
 
-python run_bwas.py
+python run_bwas.py --env cube3 --insts data/cube3/canon.pkl --heur models/cube3/current.pt --batch_size 10 --weight 0.4 --results results/cube3/
