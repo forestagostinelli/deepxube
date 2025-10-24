6353ee871778df361c3c43d5f1b178aae854f721
Forest Agostinelli <forestagostinelli@Forests-MacBook-Air.local>
2025-10-24 13:14:21 -0400

    greedy updaters [no ci]

--- deepxube/updater/updaters.py
+++ deepxube/updater/updaters.py
@@ -4,9 +4,10 @@ from deepxube.base.env import Env, EnvEnumerableActs
 from deepxube.base.heuristic import HeurNNetV, HeurNNetQ
 from deepxube.base.pathfinding import NodeV, NodeQ
 from deepxube.base.updater import UpdateHeurV, UpdateHeurQEnum, UpArgs
-from deepxube.pathfinding.q.greedy_policy_q import InstanceGrPolQ, GreedyPolicyQEnum
-from deepxube.pathfinding.v.bwas import BWASEnum, InstanceBWAS
 from deepxube.pathfinding.v.step_len_supervised_v import StepLenSupV, InstanceStepLenSup
+from deepxube.pathfinding.v.bwas import BWASEnum, InstanceBWAS
+from deepxube.pathfinding.v.greedy_policy import InstanceGrPolV, GreedyPolicyVEnum
+from deepxube.pathfinding.q.greedy_policy_q import InstanceGrPolQ, GreedyPolicyQEnum
 from deepxube.utils.timing_utils import Times
 from deepxube.pathfinding.q.bwqs import BWQSEnum, InstanceBWQS
 
@@ -38,6 +39,23 @@ class UpdateHeurBWASEnum(UpdateHeurV[EnvEnumerableActs, InstanceBWAS, BWASEnum])
                        times: Times) -> List[InstanceBWAS]:
         root_nodes: List[NodeV] = self._get_root_nodes(pathfind, steps_gen, times)
         return [InstanceBWAS(root_node, 1, 1.0, inst_info) for root_node, inst_info in
+                zip(root_nodes, inst_infos, strict=True)]
+
+
+class UpdateHeurGrPolVEnum(UpdateHeurV[EnvEnumerableActs, InstanceGrPolV, GreedyPolicyVEnum]):
+    def __init__(self, env: EnvEnumerableActs, up_args: UpArgs, heur_nnet: HeurNNetV, eps: float):
+        super().__init__(env, up_args)
+        self.set_heur_nnet(heur_nnet)
+        self.eps: float = eps
+
+    def get_pathfind(self) -> GreedyPolicyVEnum:
+        return GreedyPolicyVEnum(self.env, self.get_heur_fn())
+
+    def _get_instances(self, pathfind: GreedyPolicyVEnum, steps_gen: List[int], inst_infos: List[Any],
+                       times: Times) -> List[InstanceGrPolV]:
+        root_nodes: List[NodeV] = self._get_root_nodes(pathfind, steps_gen, times)
+
+        return [InstanceGrPolV(root_node, self.eps, inst_info) for root_node, inst_info in
                 zip(root_nodes, inst_infos, strict=True)]
 
 
