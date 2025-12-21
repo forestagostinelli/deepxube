6aa1e367e475fa6b5eb7e45e1c5591c51b49d8cb
Forest Agostinelli <forestagostinelli@Forests-MacBook-Air.local>
2025-12-20 21:05:13 -0500

    train_cli [no ci]

--- deepxube/cli.py
+++ deepxube/cli.py
@@ -2,7 +2,7 @@ from typing import List, Optional, Tuple
 import argparse
 from argparse import ArgumentParser
 
-from deepxube.train_cli import train_cli
+from deepxube.train_cli import parser_train
 from deepxube.base.domain import DomainParser
 from deepxube.base.heuristic import HeurNNetParser
 from deepxube.factories.domain_factory import get_all_domain_names, get_domain_parser
@@ -40,7 +40,7 @@ def main() -> None:
     subparsers = parser.add_subparsers(help="")
 
     parser_tr: ArgumentParser = subparsers.add_parser('train', help="")
-    _parser_train(parser_tr)
+    parser_train(parser_tr)
 
     parser_domain_info: ArgumentParser = subparsers.add_parser('domain_info', help="")
     _parser_domain_info(parser_domain_info)
@@ -51,55 +51,6 @@ def main() -> None:
     args = parser.parse_args()
 
     args.func(args)
-
-
-def _parser_train(parser: ArgumentParser) -> None:
-    parser.add_argument('--domain', type=str, required=True, help="")
-
-    parser.add_argument('--heur', type=str, required=True, help="")
-    parser.add_argument('--heur_type', type=str, required=True, help="V, QFix, QIn")
-
-    parser.add_argument('--search', type=str, required=True, help="graph, greedy, sup")
-
-    parser.add_argument('--dir', type=str, required=True, help="")
-
-    # train args
-    parser.add_argument('--batch_size', type=int, default=10000, help="")
-    parser.add_argument('--lr', type=float, default=0.001, help="")
-    parser.add_argument('--lr_d', type=float, default=0.9999993, help="")
-    parser.add_argument('--max_itrs', type=int, default=100000, help="")
-    parser.add_argument('--display', type=int, default=-1, help="")
-    parser.add_argument('--no_bal', action='store_true', default=False, help="Set for no balancing")
-
-    # updater args
-    parser.add_argument('--procs', type=int, default=1, help="")
-    parser.add_argument('--step_max', type=int, required=True, help="")
-    parser.add_argument('--up_itrs', type=int, default=100, help="")
-    parser.add_argument('--search_itrs', type=int, default=1000, help="")
-    parser.add_argument('--up_batch_size', type=int, default=100, help="")
-    parser.add_argument('--up_nnet_batch_size', type=int, default=20000, help="")
-    parser.add_argument('--sync_main', action='store_true', default=False, help="")
-    parser.add_argument('--up_v', action='store_true', default=False, help="")
-
-    # update heur args
-    parser.add_argument('--backup', type=int, default=-1,
-                        help="1 for Bellman backup, -1 for limited horizon bellman lookahead (LHBL)")
-
-    # update graph search args
-    parser.add_argument('--search_weight', type=int, default=1, help="")
-    parser.add_argument('--search_eps', type=float, default=0.1, help="")
-
-    # update greedy policy args
-    parser.add_argument('--search_temp', type=float, default=1, help="")
-
-    # test args
-    parser.add_argument('--t_search_itrs', type=int, default=1000, help="")
-    parser.add_argument('--t_up_freq', type=int, default=10, help="")
-
-    # other
-    parser.add_argument('--rb', type=int, default=1, help="")
-    parser.add_argument('--debug', action='store_true', default=False, help="")
-    parser.set_defaults(func=train_cli)
 
 
 def _parser_domain_info(parser: ArgumentParser) -> None:
--- deepxube/train_cli.py
+++ deepxube/train_cli.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import argparse
+from argparse import ArgumentParser
 
 from deepxube.factories.updater_factory import get_updater
 
@@ -12,6 +13,55 @@ from deepxube.utils.command_line_utils import get_domain_from_arg, get_heur_nnet
 
 import os
 import pickle
+
+
+def parser_train(parser: ArgumentParser) -> None:
+    parser.add_argument('--domain', type=str, required=True, help="")
+
+    parser.add_argument('--heur', type=str, required=True, help="")
+    parser.add_argument('--heur_type', type=str, required=True, help="V, QFix, QIn")
+
+    parser.add_argument('--search', type=str, required=True, help="graph, greedy, sup")
+
+    parser.add_argument('--dir', type=str, required=True, help="")
+
+    # train args
+    parser.add_argument('--batch_size', type=int, default=10000, help="")
+    parser.add_argument('--lr', type=float, default=0.001, help="")
+    parser.add_argument('--lr_d', type=float, default=0.9999993, help="")
+    parser.add_argument('--max_itrs', type=int, default=100000, help="")
+    parser.add_argument('--display', type=int, default=-1, help="")
+    parser.add_argument('--no_bal', action='store_true', default=False, help="Set for no balancing")
+
+    # updater args
+    parser.add_argument('--procs', type=int, default=1, help="")
+    parser.add_argument('--step_max', type=int, required=True, help="")
+    parser.add_argument('--up_itrs', type=int, default=100, help="")
+    parser.add_argument('--search_itrs', type=int, default=1000, help="")
+    parser.add_argument('--up_batch_size', type=int, default=100, help="")
+    parser.add_argument('--up_nnet_batch_size', type=int, default=20000, help="")
+    parser.add_argument('--sync_main', action='store_true', default=False, help="")
+    parser.add_argument('--up_v', action='store_true', default=False, help="")
+
+    # update heur args
+    parser.add_argument('--backup', type=int, default=-1,
+                        help="1 for Bellman backup, -1 for limited horizon bellman lookahead (LHBL)")
+
+    # update graph search args
+    parser.add_argument('--search_weight', type=int, default=1, help="")
+    parser.add_argument('--search_eps', type=float, default=0.0, help="")
+
+    # update greedy policy args
+    parser.add_argument('--search_temp', type=float, default=1, help="")
+
+    # test args
+    parser.add_argument('--t_search_itrs', type=int, default=1000, help="")
+    parser.add_argument('--t_up_freq', type=int, default=10, help="")
+
+    # other
+    parser.add_argument('--rb', type=int, default=1, help="")
+    parser.add_argument('--debug', action='store_true', default=False, help="")
+    parser.set_defaults(func=train_cli)
 
 
 def train_cli(args: argparse.Namespace) -> None:
