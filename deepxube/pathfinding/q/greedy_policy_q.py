1d0d0514e02ff1d6ce74667eb49a313a8d259b3f
Forest Agostinelli <forestagostinelli@Forests-MacBook-Air.local>
2025-12-23 17:09:06 -0500

    greedy_q [no ci]

--- deepxube/pathfinding/q/greedy_policy_q.py
+++ deepxube/pathfinding/q/greedy_policy_q.py
@@ -1,6 +1,6 @@
 from typing import List, Any
 from abc import ABC
-from deepxube.base.domain import ActsEnum, State, Goal
+from deepxube.base.domain import ActsEnum, Action
 from deepxube.base.pathfinding import E, Instance, NodeQ, PathFindQ, Edge, PathFindQExpandEnum
 from deepxube.utils.misc_utils import boltzmann
 import numpy as np
@@ -11,16 +11,16 @@ import time
 class InstanceGrPolQ(Instance[NodeQ]):
     def __init__(self, root_node: NodeQ, temp: float, eps: float, inst_info: Any):
         super().__init__(root_node, inst_info)
-        self.curr_node: NodeQ = self.root_node
+        self.curr_edge: Edge = Edge(self.root_node, None, self.root_node.heuristic)
         self.temp: float = temp
         assert (eps >= 0.0) and (eps <= 1.0)
         self.eps: float = eps
 
-    def record_goal(self) -> None:
-        assert self.curr_node.is_solved is not None
-        if self.curr_node.is_solved:
-            if (self.goal_node is None) or (self.goal_node.path_cost > self.curr_node.path_cost):
-                self.goal_node = self.curr_node
+    def record_goal(self, node: NodeQ) -> None:
+        assert node.is_solved is not None
+        if node.is_solved:
+            if (self.goal_node is None) or (self.goal_node.path_cost > node.path_cost):
+                self.goal_node = node
 
     def finished(self) -> bool:
         return self.has_soln()
@@ -34,27 +34,28 @@ class GreedyPolicyQ(PathFindQ[E, InstanceGrPolQ], ABC):
             self.itr += 1  # TODO make more elegant
             return []
 
-        # get curr nodes
-        nodes_curr: List[NodeQ] = [inst.curr_node for inst in instances]
+        # get curr edges
+        edges: List[Edge] = [inst.curr_edge for inst in instances]
+
+        # next node
+        nodes_next_l: List[List[NodeQ]] = self.get_next_nodes(instances, [[edge] for edge in edges])
+        nodes_next: List[NodeQ] = [nodes_next_i[0] for nodes_next_i in nodes_next_l]
 
         # is solved
         start_time = time.time()
-        self.set_is_solved(nodes_curr)
-        for instance in instances:
-            instance.record_goal()
+        self.set_is_solved(nodes_next)
+        for instance, node_next in zip(instances, nodes_next):
+            instance.record_goal(node_next)
         self.times.record_time("is_solved", time.time() - start_time)
-
-        # get qvals and actions
-        start_time = time.time()
-        states: List[State] = [node.state for node in nodes_curr]
-        goals: List[Goal] = [node.goal for node in nodes_curr]
-        q_vals_l, actions_l = self.get_qvals_acts(states, goals)
-        self.times.record_time("heur", time.time() - start_time)
 
         # sample next actions
         start_time = time.time()
-        edges: List[Edge] = []
-        for q_vals, actions, node, instance in zip(q_vals_l, actions_l, nodes_curr, instances, strict=True):
+        edges_next: List[Edge] = []
+        for node_next, instance in zip(nodes_next, instances, strict=True):
+            actions: List[Action] = node_next.actions
+            assert node_next.q_values is not None
+            q_vals: List[float] = node_next.q_values
+
             next_idx: int
             if random.random() < instance.eps:
                 next_idx = random.randrange(0, len(actions))
@@ -63,19 +64,16 @@ class GreedyPolicyQ(PathFindQ[E, InstanceGrPolQ], ABC):
                 next_idx = int(np.random.multinomial(1, np.array(probs)).argmax())
             else:
                 next_idx = int(np.argmin(q_vals))
-            edges.append(Edge(node, actions[next_idx], q_vals[next_idx]))
+            edges_next.append(Edge(node_next, actions[next_idx], q_vals[next_idx]))
 
         self.times.record_time("samp_acts", time.time() - start_time)
 
         # take actions
-        nodes_next_l: List[List[NodeQ]] = self.get_next_nodes(instances, [[nodeq_act] for nodeq_act in edges])
-        for nodes_next, instance in zip(nodes_next_l, instances, strict=True):
-            assert len(nodes_next) == 1
-            instance.curr_node = nodes_next[0]
+        for edge_next, instance in zip(edges_next, instances, strict=True):
+            instance.curr_edge = edge_next
+            instance.itr += 1
 
         self.itr += 1
-        for instance in instances:
-            instance.itr += 1
 
         return edges
 
