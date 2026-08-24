a3a3d0b4dbe586b37fe7a6e21cdbc92192e10ac2
Forest Agostinelli <forestagostinelli@Forests-MacBook-Air.local>
2026-08-23 20:19:00 -0400

    docs/tutorial update [no ci]

--- tutorial/make_gridtut_prob_insts.py
+++ tutorial/make_gridtut_prob_insts.py
@@ -8,7 +8,7 @@ def main():
     data['states'] = [GridState(0,0), GridState(1,1)]
     data['goals'] = [GridGoal(6,6), GridGoal(5,5)]
 
-    pickle.dump(data, open("tutorial/grid_tut/custom_insts.pkl", "wb"), protocol=-1)
+    pickle.dump(data, open("results/grid_tut/custom_insts.pkl", "wb"), protocol=-1)
 
 
 if __name__ == "__main__":
